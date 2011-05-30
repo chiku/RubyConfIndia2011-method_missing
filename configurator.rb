4bfc6e45ad5ffea31d4ab8b8b3e5b9261527b356
Chirantan Mitra <chirantan.mitra@gmail.com>
2011-05-31 00:11:18 +0530

    Stage 4 - Prefer simplicity - Use simple hash lookups

--- configurator.rb
+++ configurator.rb
@@ -1,14 +1,16 @@
-require 'forwardable'
-require 'ostruct'
-
 module Configurator
   class << self
 
-    extend Forwardable
-    def_delegators :@configuration, :foo, :bar
-
     def config_hash(hash={'foo' => 'foo', 'bar' => 'bar'})
-      @configuration = OpenStruct.new hash
+      @configuration = hash
+    end
+
+    def foo
+      @configuration['foo']
+    end
+
+    def bar
+      @configuration['bar']
     end
   end
 end
