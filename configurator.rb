11d45f9b5a00dd0ef5cdf9e7277e236229e6efce
Chirantan Mitra <chirantan.mitra@gmail.com>
2011-05-31 00:09:48 +0530

    Express intent - use delegation provided by Forwardable

--- configurator.rb
+++ configurator.rb
@@ -1,25 +1,14 @@
+require 'forwardable'
+require 'ostruct'
+
 module Configurator
   class << self
+
+    extend Forwardable
+    def_delegators :@configuration, :foo, :bar
+
     def config_hash(hash={'foo' => 'foo', 'bar' => 'bar'})
-      @configuration = hash
-    end
-
-    def method_missing(method_id, &block)
-      config_option = method_id.to_s
-      if self.argument_valid?(config_option)
-        @configuration[config_option]
-      else
-        super
-      end
-    end
-
-    def respond_to?(method_id)
-      config_option = method_id.to_s
-      argument_valid?(config_option) || super
-    end
-
-    def argument_valid?(argument)
-      ['foo', 'bar', 'baz', 'qux', 'quux', 'corge'].include?(argument)
+      @configuration = OpenStruct.new hash
     end
   end
 end
